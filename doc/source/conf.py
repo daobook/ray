bea5ad8955d0bbd5dd398704025ebb3a748999b8
apg <znjsapg@apg.cn>
2021-11-24 09:47:38 +0800

    中文版

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -232,7 +232,10 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'zh_CN'
+# Sphinx document translation with sphinx gettext feature uses these settings:
+locale_dirs = ['../locales/']
+gettext_compact = False
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
